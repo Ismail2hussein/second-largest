cb735e5185d736a52a3944094fa7102681f8f378
Ismail2hussein <ihusseinmohamed629@gmail.com>
2023-06-14 13:56:51 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-// Good Luck 💪🏾
+function secondLargest(arr) {
+  arr.sort(function(a, b) {
+    return b - a;
+  });
+  return arr[1];
+}
+console.log(secondLargest([23, 66, 58, 12, 6])); // Output: 58
+console.log(secondLargest([65, 34, 10, 22, 85])); // Output: 65
+console.log(secondLargest([67, 22, 49, 12, 80])); // Output: 67
